bed21401a755c4991fe2032ecb16c5c0e4603ff4
Zhussupov Zhassulan <zhzhussupovkz@gmail.com>
2013-12-07 16:28:55 +0600

    add Db class

---
+++ env/db.rb
@@ -0,0 +1,50 @@
+=begin
+/**
+* @author zhzhussupovkz@gmail.com
+* @copyright (c) 2013 Zhussupov Zhassulan zhzhussupovkz@gmail.com
+*/
+=end
+#class Db
+require "sqlite3"
+
+class Db
+
+  def initialize
+    @db = SQLite3::Database.open(File.dirname(__FILE__) + "/../db/mouse.db")
+    @table = 'game'
+  end
+
+  #save game to db
+  def save_game name, score, level
+    begin
+      @db.execute "INSERT INTO " + @table + "(name, score, level) VALUES (#{name}, #{score}, #{level})"
+
+    rescue SQLite3::Exception => e
+      puts "Database Exception occured"
+      puts e
+    
+    ensure
+      db.close if db
+    end
+  end
+
+  #load game from db
+  def load_game
+    begin
+      id = db.last_insert_row_id
+      stm = @db.prepare "SELECT * FROM " + @table + "WHERE id = #{id}"
+      rs = stm.execute
+
+      row = rs.next
+
+    rescue SQLite3::Exception => e
+      puts "Database Exception occured"
+      puts e
+    
+    ensure
+      stm.close if stm
+      db.close if db
+    end
+  end
+
+end
